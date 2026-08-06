a79ceb61683b2165f782ccaae3bb25ebd3eb145b
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-06 20:00:09 +0300

    first push

---
+++ first-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+// Write your React code below!
+import './App.css'
+function App() {
+  const username = "Skate"
+  return (
+    <>
+      <h1 className='header'>Hello, {username} !</h1>
+    </>
+  )
+}
+export default App
+
+
