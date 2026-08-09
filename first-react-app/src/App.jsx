fa2f82f1d1b0523352ed36a3172814c4c65c5c91
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-09 19:30:26 +0300

    cards-grid as a className

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,16 +1,26 @@
 // Write your React code below!
 import './App.css'
 function UserCard(props) {
-
+  return (
+    <div className='card'>
+      <h2>User: {props.name}</h2>
+      <p>score: {props.score}</p>
+    </div>
+  )
 }
 
 function App() {
-  const username = "Skate"
+  const username = "gibbrishian"
   const score = 100
   return (
     <>
       <h1 className='header'>Hello, {username} !</h1>
       <p className='subtitle'> Score: {score} |  Next Level: {score + 50}</p>
+      <div className="cards-grid">
+        <UserCard name="new user" score={300} />
+        <UserCard name="user2" score={450} />
+        <UserCard name="user3" score={0} />
+      </div>
     </>
   )
 }
