ed4861ff87732dd1d6c0d723731be83c365cc9d5
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-09 15:18:52 +0300

    idr

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,10 +1,16 @@
 // Write your React code below!
 import './App.css'
+function UserCard(props) {
+
+}
+
 function App() {
   const username = "Skate"
+  const score = 100
   return (
     <>
       <h1 className='header'>Hello, {username} !</h1>
+      <p className='subtitle'> Score: {score} |  Next Level: {score + 50}</p>
     </>
   )
 }
