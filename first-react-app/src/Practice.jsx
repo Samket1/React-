91e454b03f7142dd6ed5f48e3c667208e9f80698
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-16 01:29:41 +0300

    a lot of confusin stuffs

--- first-react-app/src/Practice.jsx
+++ first-react-app/src/Practice.jsx
@@ -1,9 +1,29 @@
 import './App.css'
-
+function CharcterCard(props) {
+    return (
+        <div className='card'>
+            <h3>Name: {props.name}</h3>
+            <p>Power: {props.power}</p>
+            <p>Level: {props.level}</p>
+        </div>
+    )
+}
 function Practice() {
+    const characters = [
+        { id: 1, name: "Goku", power: "Kamehameha", level: 9000 },
+        { id: 2, name: "Naruto", power: "Rasengan", level: 8500 },
+        { id: 3, name: "Lufy", power: "Gear 5", level: 8800 },
+
+
+    ]
     return (
         <div className="container">
             <h1 className="header">Practice Mode 🚀</h1>
+            <div className='cards-grid'>
+                {characters.map((c) =>
+                    <CharcterCard key={c.id} name={c.name} power={c.power} level={c.level} />
+                )}
+            </div>
         </div>
     )
 }
