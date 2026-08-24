7eac9b4ce47b18d1ea4e94c1ce97017e09fa6d71
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-25 00:37:38 +0300

    feat: initialize React project with StateChallenge component as the main entry point

---
+++ first-react-app/src/StateChallenge.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { useState } from 'react'
+function StateChallenge() {
+  return (
+    <div className="container">
+      <h1 className="header">React State Master Challenge ⚡</h1>
+    </div>
+  )
+}
+
+export default StateChallenge
--- first-react-app/src/main.jsx
+++ first-react-app/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import StateDeepDive from './StateDeepDive.jsx'
+import StateChallenge from './StateChallenge.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <StateDeepDive />
+    <StateChallenge />
   </StrictMode>,
 )
