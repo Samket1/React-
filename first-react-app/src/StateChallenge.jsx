62703ecc40a0a76b383eab3c9e53ec5d6c75b04e
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-27 02:06:24 +0300

    feat: create StateChallenge component with interactive quantity increment and decrement buttons

--- first-react-app/src/StateChallenge.jsx
+++ first-react-app/src/StateChallenge.jsx
@@ -1,9 +1,19 @@
 import './App.css'
 import { useState } from 'react'
 function StateChallenge() {
+  const [quantity, setQuantity] = useState(1)
   return (
     <div className="container">
       <h1 className="header">React State Master Challenge ⚡</h1>
+      <div class='card'>
+        <h2>🎧 Wireless Headphones</h2>
+        <p>Quantity: {quantity}</p>
+        <p>Total Price: ${quantity * 25}</p>
+        <button onclick={(() => setQuantity(quantity + 1))}>+</button>
+
+        <button onclick={(() => setQuantity(quantity - 1))}>-</button>
+
+      </div>
     </div>
   )
 }
