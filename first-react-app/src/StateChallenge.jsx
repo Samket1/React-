b1625fac2ffbc107c7e4cc6ad75643e0435ed971
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-27 03:09:20 +0300

    useState pretty good now ig

--- first-react-app/src/StateChallenge.jsx
+++ first-react-app/src/StateChallenge.jsx
@@ -2,20 +2,26 @@ import './App.css'
 import { useState } from 'react'
 function StateChallenge() {
   const [quantity, setQuantity] = useState(1)
+  const [showSpecs, setShowSpec] = useState(false)
+  const [isDarkMode, setIsDarkMode] = useState(false)
   return (
-    <div className="container">
+    <div className={`container ${isDarkMode ? "page-dark" : "page-light"}`}>
+      <button onClick={(() => setIsDarkMode(!isDarkMode))}>{isDarkMode ? 'Light' : 'Dark'}</button>
+
       <h1 className="header">React State Master Challenge ⚡</h1>
-      <div class='card'>
+      <div class={isDarkMode ? 'card' : 'card-light'}>
         <h2>🎧 Wireless Headphones</h2>
         <p>Quantity: {quantity}</p>
         <p>Total Price: ${quantity * 25}</p>
-        <button onclick={(() => setQuantity(quantity + 1))}>+</button>
+        <button onClick={(() => setQuantity(quantity + 1))}>+</button>
+        <button onClick={(() => { setQuantity(quantity > 0 ? quantity - 1 : 0) })}>-</button>
+        <button onClick={(() => setShowSpec(!showSpecs))}>{showSpecs ? 'Hide' : 'Show'}</button>
+        {showSpecs && <subtitle> adbwabcdiahbwcdipahw </subtitle>}
 
-        <button onclick={(() => setQuantity(quantity - 1))}>-</button>
+
 
       </div>
     </div>
   )
 }
-
 export default StateChallenge
