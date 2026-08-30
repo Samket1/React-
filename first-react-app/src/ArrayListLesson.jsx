caece65dcfa978f9521370ae37b0b4653fb4ed68
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-30 21:19:52 +0300

    chaw for now

--- first-react-app/src/ArrayListLesson.jsx
+++ first-react-app/src/ArrayListLesson.jsx
@@ -2,9 +2,16 @@ import './App.css'
 import { useState } from 'react'
 
 function ArrayListLesson() {
+  const [tasks, setTasks] = useState(["Buy Groceries", "Finish Homework", "Hit the Gym"])
   return (
     <div className="container">
       <h1 className="header">Dynamic Tasks & Lists 📋</h1>
+      {tasks.map((task, index) => (
+        <div style={{ marginBottom: "12px" }} className='card'
+          key={index}>
+          <h3>📌 {task}</h3>
+        </div>
+      ))}
     </div>
   )
 }
