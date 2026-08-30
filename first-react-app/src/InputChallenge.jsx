e1ecb4356552fb984d21a87df93aef4863023bc0
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-08-30 18:36:47 +0300

    challenge done hehe

---
+++ first-react-app/src/InputChallenge.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import { useState } from 'react'
+
+function InputChallenge() {
+  const [author, setAuthor] = useState("")
+  const [post, setPost] = useState("")
+  const [like, setLike] = useState(false)
+
+  return (
+    <div className="container">
+      <h1 className="header">Social Post Composer Challenge 🐦✨</h1>
+      <input className="input-field" type="text" placeholder="Author's Name  (e.g. @johndoe)..."
+        value={author} onChange={(e) => setAuthor(e.target.value)} />
+      <input className="input-field" type="text" placeholder="💭What's on your mind?"
+        value={post} onChange={(e) => setPost(e.target.value)} />
+
+      <div className='card'>
+        <h2>{author ? author : "Anonymous User"}</h2>
+        <p>{post ? post : "No Post Yet"}</p>
+        <subtitle style={{ color: '#777777ff' }}>
+          Characters: {post.length < 100 ? `${post.length}/100` : 'Max Character'}
+        </subtitle>
+        <button className={like ? "btn-primary" : "btn"} onClick={() => setLike(!like)}>{like ? "❤️" : "🤍"}</button>
+        <button onClick={() => {
+          setAuthor("")
+          setPost("")
+          setLike(false)
+        }}>CLEAR</button>
+      </div>
+    </div >
+  )
+}
+
+export default InputChallenge
--- first-react-app/src/main.jsx
+++ first-react-app/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import InputLesson from './InputLesson.jsx'
+import InputChallenge from './InputChallenge.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <InputLesson />
+    <InputChallenge />
   </StrictMode>,
 )
