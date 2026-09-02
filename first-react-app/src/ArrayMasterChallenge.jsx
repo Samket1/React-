b70438fd19595ff2955d1e774f431f3fff4edd41
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-09-02 21:39:38 +0300

    filter done

--- first-react-app/src/ArrayMasterChallenge.jsx
+++ first-react-app/src/ArrayMasterChallenge.jsx
@@ -2,9 +2,21 @@ import './App.css'
 import { useState } from 'react'
 
 function ArrayMasterChallenge() {
+  const [playlist, setPlayList] = useState(["🎵 Blinding Lights", "🎸 Bohemian Rhapsody", "⚡ Thunderstruck"])
+  const [newPlaylist, setNewPlayList] = ("")
   return (
     <div className="container">
       <h1 className="header">RPG Inventory & Loot Bag Challenge 🎒⚔️</h1>
+      {playlist.map(
+        (play, index) => (
+          <div className='card' key={index}>
+            {play}
+            <button onClick={() => setPlayList(playlist.filter((_, i) => (i !== index)))}>🗑️</button>
+          </div>
+        )
+      )}
+
+
     </div>
   )
 }
