15d50cce69c08049acccb408b4db97b429307eff
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-09-02 21:09:40 +0300

    challenge 1 completed

---
+++ first-react-app/src/ArrayMasterChallenge.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { useState } from 'react'
+
+function ArrayMasterChallenge() {
+  return (
+    <div className="container">
+      <h1 className="header">RPG Inventory & Loot Bag Challenge 🎒⚔️</h1>
+    </div>
+  )
+}
+
+export default ArrayMasterChallenge
--- first-react-app/src/main.jsx
+++ first-react-app/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import DynamicListLesson from './DynamicListLesson.jsx'
+import ArrayMasterChallenge from './ArrayMasterChallenge.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DynamicListLesson />
+    <ArrayMasterChallenge />
   </StrictMode>,
 )
