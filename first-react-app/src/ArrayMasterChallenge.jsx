02ff0e5c1a0ca79c16cd02ed1a5dee91a5b35e0f
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-09-02 21:47:54 +0300

    98% confident

--- first-react-app/src/ArrayMasterChallenge.jsx
+++ first-react-app/src/ArrayMasterChallenge.jsx
@@ -3,10 +3,15 @@ import { useState } from 'react'
 
 function ArrayMasterChallenge() {
   const [playlist, setPlayList] = useState(["🎵 Blinding Lights", "🎸 Bohemian Rhapsody", "⚡ Thunderstruck"])
-  const [newPlaylist, setNewPlayList] = ("")
+  const [newPlaylist, setNewPlayList] = useState("")
   return (
     <div className="container">
       <h1 className="header">RPG Inventory & Loot Bag Challenge 🎒⚔️</h1>
+      <input type="text" placeholder='New PlayList here...' value={newPlaylist} onChange={(e) => setNewPlayList(e.target.value)} />
+      <button onClick={() => {
+        setPlayList([...playlist, newPlaylist])
+        setNewPlayList('')
+      }}>Add</button>
       {playlist.map(
         (play, index) => (
           <div className='card' key={index}>
