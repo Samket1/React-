79a8752e54de8737eab5d0cc790bf6c4aa4f8f47
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-09-02 19:05:52 +0300

    i forgot

--- first-react-app/src/DynamicListLesson.jsx
+++ first-react-app/src/DynamicListLesson.jsx
@@ -7,17 +7,25 @@ function DynamicListLesson() {
     "🟨 Modern JavaScript",
     "🎨 Vanilla CSS"
   ])
-
+  const [newSkill, setNewSkill] = useState("")
   return (
     <div className="container">
       <h1 className="header">Dynamic Interactive List ⚡📋</h1>
 
-      {/* 1. Input & Add Button will go here */}
+      <input type='text' placeholder='Add Skills' value={newSkill}
+        onChange={(e) => setNewSkill(e.target.value)} />
 
-      {/* 2. List of Cards will be mapped here */}
+      <button className='btn' onClick={() => {
+        setSkills([...skills, newSkill])
+        setNewSkill('')
+      }}>Add</button>
+
+
       {skills.map((skill, index) => (
         <div className="card" style={{ marginTop: "16px" }} key={index}>
           <h3>{skill}</h3>
+          <button className='btn' >  🗑️ Delete
+          </button>
         </div>
       ))}
     </div>
