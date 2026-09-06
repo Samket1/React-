89818a92bc265a0a6d9af6224ec94f65d858d2bb
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-09-06 14:02:54 +0300

    feat: add EffectLesson component to fetch and display Pokémon data from PokeAPI using useEffect

--- first-react-app/src/EffectLesson.jsx
+++ first-react-app/src/EffectLesson.jsx
@@ -2,40 +2,48 @@ import './App.css'
 import { useState, useEffect } from 'react'
 
 function EffectLesson() {
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [city, setCity] = useState("")
-  const [isLoading, setIsLoading] = useState(true)
-  const [id, setId] = useState(1)
-  const [avatar, setAvatar] = useState("")
-  const [username, setUsername] = useState("Samket1")
-
+  const [pokemon, setPokemon] = useState("")
+  const [imgp, setImgp] = useState("")
+  const [id, setId] = useState("1")
+  const [search, setSearch] = useState("")
+  const [er, setEr] = useState("")
   useEffect(() => {
-    setIsLoading(true)
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setName(data.name)
-        setEmail(data.email)
-        setCity(data.address.city)
-        setIsLoading(false)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("HTTP Error " + res.status)
+        }
+        return (res.json())
       })
+      .then(data => {
+        setPokemon(data.name)
+        setImgp(data.sprites.front_default)
+        setEr("")
 
-
-  }
-    , [id])
-
+      })
+      .catch(err => {
+        setPokemon("Creature Not Found ❌")
+        setImgp("")
+        setEr(err.message)
+      })
+  }, [id]
+  )
   return (
     <div className="cyber-container">
-      <h1 className="header">Lesson 6: useEffect ⚡</h1>
-      {isLoading && <p>📡 Downloading Intel...</p>}
-      <h2 style={{ textAlign: "right" }}>{name}</h2>
-      <p style={{ textAlign: "right" }}> email: <span style={{ color: '#00f0ff' }}>{email}</span></p>
-      <p style={{ textAlign: "right" }}> city: <span style={{ color: '#d4ff00ff' }}>{city}</span></p>
-      <input type="number" placeholder='🔍Search...' value={id} onChange={(e) => setId(e.target.value)} />
-    </div>
+      <h1 className="header">Lesson 6: useEffect & APIs ⚡</h1>
+      <input type="text" placeholder='1,2,3,4... or charmeleon, ivysaur
 
+...' value={search} onChange={(e) => setSearch(e.target.value)} />
+      <button onClick={() => setId(search)}>Search</button>
+
+      <div className='cyber-container'>
+        <img className="card" src={imgp} alt=".   No Creature To Display" style={{ width: "100px", height: "100px" }} />
+        <p className='hud-badge'>Name: {pokemon}</p>
+        <p className='hud-badge'>ID: {id}</p>
+        <p className='ifZero'>{er}</p>
+      </div>
+    </div>
   )
 }
 
---
+++ first-react-app/src/RickMortyChallenge.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+
+function RickMortyChallenge() {
+  return (
+    <div className="cyber-container">
+      <h1 className="header">Multiverse Scanner 🌀🧪</h1>
+    </div>
+  )
+}
+
+export default RickMortyChallenge
