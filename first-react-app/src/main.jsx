836a76c2d2ec1ba08d2b03d2f2bc164655036740
Snipkekt <146439025+Snipkekt@users.noreply.github.com>
2026-09-06 14:04:11 +0300

    feat: initialize main.jsx with RickMortyChallenge component rendering

--- first-react-app/src/main.jsx
+++ first-react-app/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import EffectLesson from './EffectLesson.jsx'
+import RickMortyChallenge from './RickMortyChallenge.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <EffectLesson />
+    <RickMortyChallenge />
   </StrictMode>,
 )
